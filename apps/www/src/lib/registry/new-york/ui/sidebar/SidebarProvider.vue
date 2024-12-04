8a24d11a659b4d74d18ed90032ede8b6418fe46c
Fabian Bernhart <135550293+fabianbernhart@users.noreply.github.com>
2024-12-04 20:06:12 +0100

    fix: add missing `useMediaQuery` in `SidebarProvider.vue` new-york style (#927)

--- apps/www/src/lib/registry/new-york/ui/sidebar/SidebarProvider.vue
+++ apps/www/src/lib/registry/new-york/ui/sidebar/SidebarProvider.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils'
-import { useEventListener, useVModel } from '@vueuse/core'
+import { useEventListener, useMediaQuery, useVModel } from '@vueuse/core'
 import { TooltipProvider } from 'radix-vue'
 import { computed, type HTMLAttributes, type Ref, ref } from 'vue'
 import { provideSidebarContext, SIDEBAR_COOKIE_MAX_AGE, SIDEBAR_COOKIE_NAME, SIDEBAR_KEYBOARD_SHORTCUT, SIDEBAR_WIDTH, SIDEBAR_WIDTH_ICON } from './utils'
