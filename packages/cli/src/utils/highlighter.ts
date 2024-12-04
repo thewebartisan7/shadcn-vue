3f0ded8e7f52774111f18d5ae5aa0c742bd9bcd2
Sadegh Barati <sadeghbaratiwork@gmail.com>
2024-12-04 23:24:33 +0330

    refactor: replace `kleur` with `consola` colors

--- packages/cli/src/utils/highlighter.ts
+++ packages/cli/src/utils/highlighter.ts
@@ -1,8 +1,8 @@
-import { cyan, green, red, yellow } from 'kleur/colors'
+import { colors } from 'consola/utils'
 
 export const highlighter = {
-  error: red,
-  warn: yellow,
-  info: cyan,
-  success: green,
+  error: colors.red,
+  warn: colors.yellow,
+  info: colors.cyan,
+  success: colors.green,
 }
